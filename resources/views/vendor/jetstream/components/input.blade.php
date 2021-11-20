4213129a373694d738dabba56b747c337797429b
matt1007k <matt1007k@gmail.com>
2021-11-19 21:24:19 -0500

    Text base on input component

--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,6 +1,6 @@
 @props(['disabled' => false])
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->class(['border-gray-300 focus:border-indigo-300
-focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm', 'border-red-500' =>
+focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm text-base', 'border-red-500' =>
 $errors->has($attributes->get('name'))]) !!}
 />
